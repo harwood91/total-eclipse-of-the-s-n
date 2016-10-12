123361b6a37b97b006ad80c286890b19cb5891b8
Andrew Harwood <Harwood.91@hotmail.co.uk>
2016-10-12 20:16:12 +0100

    contact page and contact form working

--- wp-content/themes/teots/template-contact.php
+++ wp-content/themes/teots/template-contact.php
@@ -5,13 +5,77 @@
  * @package teots
  */
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $to = 'andrew@drawandcode.com';
-    $subject = 'email test';
-    $body = 'The email body content';
-    $headers = array('Content-Type: text/html; charset=UTF-8');
+require 'inc/class.phpmailer.php';
+require 'inc/class.smtp.php';
 
-    wp_mail( $to, $subject, $body, $headers );
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //print_r($_POST);
+
+    //Do form validation
+    if(!empty($_POST['contactName'])) {
+        if(!empty($_POST['email']) && is_email($_POST['email'])) {
+            if(!empty($_POST['message'])) {
+
+                $msg = '<h1>Website enquiry:</h1><table><tr><td>Name:</td><td>';
+                $msg .= $_POST['contactName'];
+                $msg .= '</td></tr><tr><td>Email address:</td><td>' . $_POST['email'];
+                $msg .= '</td></tr><tr><td>Message:</td><td>' . $_POST['message'];
+
+                //Create a new PHPMailer instance
+                $mail = new PHPMailer();
+                $mail->IsSMTP();
+                $mail->Host = 'smtp.mandrillapp.com';
+                $mail->Port = 587;
+                $mail->SMTPAuth = true;
+                $mail->Username = 'chris@drawandcode.com';
+                $mail->Password = '8d162f7a-c9ca-4f7c-968d-fd7e0389c66c';
+
+
+                //Set who the message is to be sent from
+                $mail->SetFrom($_POST['email'], $_POST['contactName']);
+                $mail->AddAddress('andrew@drawandcode.com', 'Test');
+
+
+                $mail->Subject = 'TEOTS - Enquiry';
+                //Read an HTML message body from an external file, convert referenced images to embedded, convert HTML into a basic plain-text alternative body
+                $mail->MsgHTML($msg);
+
+                //Send the message, check for errors
+
+                if(!$mail->Send()) {
+
+                    //$mailFeedback = '<div class="alert alert-danger">Sorry, there was an error sending your message. Please call us on 0151 601 9479</div>';
+                    $mailFeedback .= '<div class="feedbackError">' . $mail->ErrorInfo . '</div>';
+                    //echo '<div class="alert alert-danger">Sorry, there was an error sending your message. Please call us on 0151 601 9479</div>';
+                } else {
+                    //header("Location: http://dev.to2c:81/contact/");
+                    $mailFeedback = '<div class="alert alert-success">Thank you, we have received your message and will be back in touch as soon as possible.</div>';
+                    //echo '<div class="alert alert-success" style="margin-bottom:0"><div class="container">Thank you, we have received your message and will be back in touch as soon as possible.</div></div>';
+                }
+
+            } else {
+                $mailFeedback = '<div class="alert alert-danger">Sorry, there was an error sending your message. Please do not send an empty message</div>';
+            }
+        } else {
+            $mailFeedback = '<div class="alert alert-danger">Sorry, there was an error sending your message. Please enter a valid email address</div>';
+        }
+    } else {
+        $mailFeedback = '<div class="alert alert-danger">Sorry, there was an error sending your message. Please enter your full name</div>';
+    }
+
+} else {
+    $mailFeedback = '';
+}
+
+if (isset($_GET['success'])) {
+    $success = $_GET['success'];
+
+    if($success === 1) {
+        $mailFeedback = '<div class="alert alert-success" style="margin-bottom:0">Thank you, we have received your message and will be back in touch as soon as possible.</div>';
+    }
+
+    echo $_GET['link'];
 }
 
 get_header(); ?>
@@ -19,54 +83,57 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-            <div class="contactUs">
-                <h3>Contact Us</h3>
-                <form style="max-width: 100%" class="clearfix" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" name="contactName" class="form-control" id="name" placeholder="Name">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" name="message" rows="5" placeholder="Write your enquiry here"></textarea>
-                    </div>
-                    <button type="submit" value="submit" class="btn btn-default btn-cakes float-right">Submit Enquiry</button>
-                </form>
+            <div class="contactHead">
+                <div class="container">
+                    <h1>Contact Us</h1>
+                    <p>If you have any questions or want to find out more about our campaign please don't hesitate to contact us! Simply fill in the form below and we'll get back to you asap!</p>
+                </div>
             </div>
 
+            <div class="contactForm">
+                <div class="container">
+                    <div class="col-sm-6">
+                        <div class="formSection">
 
-            <?php
-            if ( have_posts() ) :
+                            <h2>Contact Form</h2>
+                            <p>If you wish to contact us please fill in the form below and we'll get back to you as soon as possible!</p>
 
-                if ( is_home() && ! is_front_page() ) : ?>
-                    <header>
-                        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                    </header>
+                            <form class="form form-horizontal" role="form" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+                                <div class="form-group">
+                                        <input type="text" class="form-control" id="name" name="contactName" placeholder="First & Last Name" value="">
+                                </div>
+                                <div class="form-group">
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+                                </div>
+                                <div class="form-group">
+                                        <textarea class="form-control" placeholder="Enter your message here ..." rows="4" name="message"></textarea>
+                                </div>
+                                <div class="form-group">
+                                        <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary teotsButton large">
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-sm-10 col-sm-offset-2">
+                                        <! Will be used to display an alert to the user>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
 
-                    <?php
-                endif;
-
-                /* Start the Loop */
-                while ( have_posts() ) : the_post();
-
-                    /*
-                     * Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    get_template_part( 'template-parts/content', get_post_format() );
-
-                endwhile;
-
-
-            else :
-
-                get_template_part( 'template-parts/content', 'none' );
-
-            endif; ?>
+                    <div class="col-sm-6">
+                        <div class="otherContact">
+                            <h2>Social Media</h2>
+                            <p>If you would rather contact us via social media please follow us on twitter or join our campaign group on facebook.</p>
+                            <div class="sm">
+                                <i class="fa fa-twitter" aria-hidden="true"></i> <a href="https://twitter.com/totaleclipse96">@totaleclipse96</a>
+                            </div>
+                            <div class="sm">
+                                <i class="fa fa-facebook" aria-hidden="true"></i> <a href="https://www.facebook.com/totaleclipseofthesunpage/?fref=ts">Total Eclipse of the S&n</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </main><!-- #main -->
     </div><!-- #primary -->
