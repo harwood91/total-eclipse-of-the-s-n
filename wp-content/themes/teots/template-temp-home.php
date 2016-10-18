e65b17fe9ae0416a2287b0b8a0091d161dfe263a
Andrew Harwood <Harwood.91@hotmail.co.uk>
2016-10-18 23:25:23 +0100

    Added temp homepage (not changed settings in the db yet!)

---
+++ wp-content/themes/teots/template-temp-home.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Temporary Home Template
+ *
+ * @package teots
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-3.1.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+
+    <!-- FontAwesome -->
+    <script src="https://use.fontawesome.com/5df7820823.js"></script>
+
+    <!-- Google Maps API -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1fiJz0MgmxyHASkZk25GEr5pztoNsrrA"></script>
+
+    <?php wp_head(); ?>
+</head>
+
+<div id="content" class="site-content">
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+            <div class="mainPage">
+                <img class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/img/teots-large.png">
+
+                <div class="mainText">
+                    Total Eclipse of The S*n Website coming soon!
+                </div>
+
+                <div class="socialMediaHome">
+                    <p>Meanwhile please keep up to data with our campaign using our social media pages</p>
+                    <a href="https://www.facebook.com/groups/1576357575995356/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/totaleclipse96"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="https://plus.google.com/u/0/115392298920541165730"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/totaleclipseofthesun/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+
+            </div>
+
+        </main>
+    </div>
+</div>
